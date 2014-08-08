88f45f46afdb89e72e80991e1470c553acf3a1db
Stef Walter <stef@thewalter.net>
2014-08-08 17:40:57 +0200

    trust: Double check that index bucket is valid before access

--- trust/index.c
+++ trust/index.c
@@ -264,9 +264,9 @@ bucket_insert (index_bucket *bucket,
 		alloc = alloc ? alloc * 2 : 1;
 		return_if_fail (alloc != 0);
 		bucket->elem = realloc (bucket->elem, alloc * sizeof (CK_OBJECT_HANDLE));
-		return_if_fail (bucket->elem != NULL);
 	}
 
+	return_if_fail (bucket->elem != NULL);
 	memmove (bucket->elem + at + 1, bucket->elem + at,
 	         (bucket->num - at) * sizeof (CK_OBJECT_HANDLE));
 	bucket->elem[at] = handle;
@@ -284,9 +284,9 @@ bucket_push (index_bucket *bucket,
 		alloc = alloc ? alloc * 2 : 1;
 		return_val_if_fail (alloc != 0, false);
 		bucket->elem = realloc (bucket->elem, alloc * sizeof (CK_OBJECT_HANDLE));
-		return_val_if_fail (bucket->elem != NULL, false);
 	}
 
+	return_val_if_fail (bucket->elem != NULL, false);
 	bucket->elem[bucket->num++] = handle;
 	return true;
 }
