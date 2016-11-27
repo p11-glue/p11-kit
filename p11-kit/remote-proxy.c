0a01c7fe4e55d728713ef75c058181463665cb98
Lubomir Rintel <lkundrak@v3.sk>
2016-11-27 23:18:01 +0100

    p11-kit-remote-proxy: add a PKCS#11 module for remoting
    This allows making a PKCS#11-enabled application that doesn't link to
    p11-kit to use a remote module easily.
    
    The remote is set up using P11_REMOTE environment variable, for a lack
    of a better mechanism to pass data to the PKCS#11 module.

---
+++ p11-kit/remote-proxy.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *     * Redistributions in binary form must reproduce the
+ *       above copyright notice, this list of conditions and
+ *       the following disclaimer in the documentation and/or
+ *       other materials provided with the distribution.
+ *     * The names of contributors to this software may not be
+ *       used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * Author: Lubomir Rintel <lkundrak@v3.sk>
+ */
+
+#include "config.h"
+#include "p11-kit.h"
+
+#include <stdlib.h>
+
+CK_RV
+C_GetFunctionList (CK_FUNCTION_LIST_PTR_PTR list)
+{
+	char *remote;
+
+	remote = getenv ("P11_REMOTE");
+	if (remote == NULL)
+		return CKR_ARGUMENTS_BAD;
+
+	*list = p11_kit_module_remote (remote, 0);
+	if (*list == NULL)
+		return CKR_GENERAL_ERROR;
+
+	return CKR_OK;
+}
