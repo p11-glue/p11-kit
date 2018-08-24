0961cf527f1414bf5a900d958ee776cdd28f3525
Daiki Ueno <dueno@redhat.com>
2018-08-24 17:10:20 +0200

    travis: Manually install cpp-coveralls
    To accommodate the gcov format change in gcc 8.1:
    https://github.com/eddyxu/cpp-coveralls/pull/127
    which is not yet available in the pip version.

--- .travis/linux/after_success.sh
+++ .travis/linux/after_success.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 if test x"$COVERAGE" = xyes; then
-  docker exec $CONTAINER pip install cpp-coveralls
+  # docker exec $CONTAINER pip install cpp-coveralls
+
+  # manually install cpp-coveralls until the gcov fix has been
+  # incorporated in the pip version
+  docker exec $CONTAINER sh -c "cd /tmp && rm -rf cpp-coveralls && git clone -q https://github.com/eddyxu/cpp-coveralls && cd cpp-coveralls && python setup.py build && python setup.py install"
   docker exec \
 	 -e TRAVIS_JOB_ID="$TRAVIS_JOB_ID" \
 	 -e TRAVIS_BRANCH="$TRAVIS_BRANCH" \
-	 $CONTAINER sh -c "cd $BUILDDIR && coveralls -b $BUILDDIR -E '(^|.*/)(frob|mock|test)-.*|(^|.*/)(virtual-fixed\.c)' --gcov-options '\-lp'"
+	 $CONTAINER sh -c "cd $BUILDDIR && cpp-coveralls -b $BUILDDIR -E '(^|.*/)(frob|mock|test)-.*|(^|.*/)(virtual-fixed\.c)' --gcov-options '\-lp'"
 fi
