8cb21a6a09d18156c9002b97687e5ac0bfa0fc92
Vincent JARDIN <vjardin@free.fr>
2019-10-22 22:50:45 +0200

    Fix RPC calls: BYTE buffer not null and length 0
    Let's add a support for cases when the buffer != NULL but the
    length is 0. According to Oasis, buffer = NULL and length = 0
    means a query of the length so the subsequent calls with a
    buffer != NULL should fill buffer when length is long enough.
    If not, according to Oasis, one should get a CKR_BUFFER_TOO_SMALL.
    
    This current fix is for IN_BYTE_BUFFER(), same
    for IN_ATTRIBUTE_BUFFER().
    
    See the previous commit for IN_ULONG_BUFFER(). This patch is
    strictly using the same design pattern.
    
    Fix: issue #257
    
    Suggested-by: Daiki Ueno <dueno@redhat.com>

--- p11-kit/rpc-message.c
+++ p11-kit/rpc-message.c
@@ -336,14 +336,16 @@ p11_rpc_message_write_ulong (p11_rpc_message *msg,
 
 bool
 p11_rpc_message_write_byte_buffer (p11_rpc_message *msg,
-                                   CK_ULONG count)
+                                   CK_BYTE_PTR array,
+                                   CK_ULONG_PTR n_array)
 {
 	assert (msg != NULL);
 	assert (msg->output != NULL);
 
 	/* Make sure this is in the right order */
 	assert (!msg->signature || p11_rpc_message_verify_part (msg, "fy"));
-	p11_rpc_buffer_add_uint32 (msg->output, count);
+	p11_rpc_buffer_add_byte (msg->output, array ? 0 : 1);
+	p11_rpc_buffer_add_uint32 (msg->output, *n_array);
 	return !p11_buffer_failed (msg->output);
 }
 
