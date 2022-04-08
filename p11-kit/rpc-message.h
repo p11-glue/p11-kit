7235af663b95e3e0a7c035d3de7f26c0c6a4810e
Ingo Franzki <ifranzki@linux.ibm.com>
2022-04-08 13:02:41 +0200

    Add support for MAC and HMAC general mechanisms
    The take a mechanism parameter of type CK_MAC_GENERAL_PARAMS which
    is a CK_ULONG.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- p11-kit/rpc-message.h
+++ p11-kit/rpc-message.h
@@ -520,4 +520,15 @@ bool		p11_rpc_buffer_get_des_iv_mechanism_value (p11_buffer *buffer,
 							   void *value,
 							   CK_ULONG *value_length);
 
+void		p11_rpc_buffer_add_mac_general_mechanism_value
+							  (p11_buffer *buffer,
+							   const void *value,
+							   CK_ULONG value_length);
+
+bool		p11_rpc_buffer_get_mac_general_mechanism_value
+							  (p11_buffer *buffer,
+							   size_t *offset,
+							   void *value,
+							   CK_ULONG *value_length);
+
 #endif /* _RPC_MESSAGE_H */
