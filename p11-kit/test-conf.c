36709702edf92f9baa9c043f55b2566f8ca2dd01
Daiki Ueno <dueno@redhat.com>
2023-04-12 15:07:55 +0900

    tests: Fix mingw test failures
    This updates the module configuration files in the Windows fixture
    directory with Unix version.
    
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- p11-kit/test-conf.c
+++ p11-kit/test-conf.c
@@ -270,9 +270,9 @@ test_load_modules_merge (void)
 	p11_message_clear ();
 
 	configs = _p11_conf_load_modules (CONF_USER_MERGE,
-	                                  SRCDIR "/p11-kit/fixtures/package-modules",
-	                                  SRCDIR "/p11-kit/fixtures/system-modules",
-	                                  SRCDIR "/p11-kit/fixtures/user-modules");
+	                                  P11_PACKAGE_CONFIG_MODULES,
+	                                  P11_SYSTEM_CONFIG_MODULES,
+	                                  P11_USER_CONFIG_MODULES);
 	assert_ptr_not_null (configs);
 	assert (assert_msg_contains (p11_message_last (), "invalid config filename"));
 
@@ -303,9 +303,9 @@ test_load_modules_user_none (void)
 	p11_message_clear ();
 
 	configs = _p11_conf_load_modules (CONF_USER_NONE,
-	                                  SRCDIR "/p11-kit/fixtures/package-modules",
-	                                  SRCDIR "/p11-kit/fixtures/system-modules",
-	                                  SRCDIR "/p11-kit/fixtures/user-modules");
+	                                  P11_PACKAGE_CONFIG_MODULES,
+	                                  P11_SYSTEM_CONFIG_MODULES,
+	                                  P11_USER_CONFIG_MODULES);
 	assert_ptr_not_null (configs);
 	assert (assert_msg_contains (p11_message_last (), "invalid config filename"));
 
@@ -334,9 +334,9 @@ test_load_modules_user_only (void)
 	p11_message_clear ();
 
 	configs = _p11_conf_load_modules (CONF_USER_ONLY,
-	                                  SRCDIR "/p11-kit/fixtures/package-modules",
-	                                  SRCDIR "/p11-kit/fixtures/system-modules",
-	                                  SRCDIR "/p11-kit/fixtures/user-modules");
+	                                  P11_PACKAGE_CONFIG_MODULES,
+	                                  P11_SYSTEM_CONFIG_MODULES,
+	                                  P11_USER_CONFIG_MODULES);
 	assert_ptr_not_null (configs);
 	assert_ptr_eq (NULL, (void *)p11_message_last ());
 
@@ -365,8 +365,8 @@ test_load_modules_no_user (void)
 	p11_message_clear ();
 
 	configs = _p11_conf_load_modules (CONF_USER_MERGE,
-	                                  SRCDIR "/p11-kit/fixtures/package-modules",
-	                                  SRCDIR "/p11-kit/fixtures/system-modules",
+	                                  P11_PACKAGE_CONFIG_MODULES,
+	                                  P11_SYSTEM_CONFIG_MODULES,
 	                                  SRCDIR "/p11-kit/fixtures/non-existant");
 	assert_ptr_not_null (configs);
 	assert (assert_msg_contains (p11_message_last (), "invalid config filename"));
