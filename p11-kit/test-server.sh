e2a93d379537c5b10a8ab16e44dbc5a8e9d58ae6
Zoltan Fridrich <zfridric@redhat.com>
2023-06-20 11:10:49 +0200

    Add test for p11-kit-client.so
    Signed-off-by: Zoltan Fridrich <zfridric@redhat.com>

--- p11-kit/test-server.sh
+++ p11-kit/test-server.sh
@@ -23,9 +23,12 @@ unset P11_KIT_SERVER_PID
 XDG_RUNTIME_DIR="$testdir"
 export XDG_RUNTIME_DIR
 
-echo 1..4
+P11_KIT_PRIVATEDIR="${abs_top_builddir}/p11-kit"
+export P11_KIT_PRIVATEDIR
 
-"$abs_top_builddir"/p11-kit/p11-kit-server -s --provider "$P11_MODULE_PATH"/mock-one.so pkcs11: > start.env 2> start.err
+echo 1..5
+
+"$abs_top_builddir"/p11-kit/p11-kit-server-testable -s --provider "$P11_MODULE_PATH"/mock-one.so pkcs11: > start.env 2> start.err
 if test $? -eq 0; then
 	echo "ok 1 /server/start"
 else
@@ -39,15 +42,32 @@ fi
 if test "${P11_KIT_SERVER_ADDRESS+set}" = "set" && test "${P11_KIT_SERVER_PID+set}" = "set"; then
 	echo "ok 2 /server/start-env"
 else
+	pkill -f p11-kit-server
 	echo "not ok 2 /server/start-env"
 	exit 1
 fi
 
-"$abs_top_builddir"/p11-kit/p11-kit-server -s -k > stop.env 2> stop.err
-if test $? -eq 0; then
-	echo "ok 3 /server/stop"
+if p11tool --version > /dev/null; then
+	p11tool --provider "$P11_MODULE_PATH"/p11-kit-client.so --list-tokens > /dev/null 2> p11tool.err
+	if test $? -eq 0; then
+		echo "ok 3 /server/client-access"
+	else
+		pkill -f p11-kit-server
+		echo "not ok 3 /server/client-access"
+		sed 's/^/# /' start.err
+		exit 1
+	fi
 else
-	echo "not ok 3 /server/stop"
+	echo "ok 3 /server/client-access"
+	echo "cannot find p11tool" >&2
+fi
+
+"$abs_top_builddir"/p11-kit/p11-kit-server-testable -s -k > stop.env 2> stop.err
+if test $? -eq 0; then
+	echo "ok 4 /server/stop"
+else
+	pkill -f p11-kit-server
+	echo "not ok 4 /server/stop"
 	sed 's/^/# /' stop.err
 	exit 1
 fi
@@ -55,8 +75,8 @@ fi
 . ./stop.env
 
 if test "${P11_KIT_SERVER_ADDRESS-unset}" = "unset" && test "${P11_KIT_SERVER_PID-unset}" = "unset"; then
-	echo "ok 4 /server/stop-env"
+	echo "ok 5 /server/stop-env"
 else
-	echo "not ok 4 /server/stop-env"
+	echo "not ok 5 /server/stop-env"
 	exit 1
 fi
