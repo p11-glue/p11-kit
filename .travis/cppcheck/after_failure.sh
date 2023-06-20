7dbc2687364805ee6c2478fa69ecdfeb89648d61
Zoltan Fridrich <zfridric@redhat.com>
2023-06-20 17:05:08 +0200

    Remove travis configuration
    Signed-off-by: Zoltan Fridrich <zfridric@redhat.com>

--- .travis/cppcheck/after_failure.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker exec $CONTAINER su - user sh -c "cd $BUILDDIR && cat cppcheck.log"
--- .travis/freebsd/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-meson _build -Dsystemd=disabled -Dbash_completion=disabled
-meson test -C _build
