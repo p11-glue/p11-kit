a3549f82c8769f41a7cee8f2198354939e28c05c
Zoltan Fridrich <zfridric@redhat.com>
2023-08-29 14:34:55 +0200

    Dont pass NULL to p11_kit_modules_finalize/release
    Signed-off-by: Zoltan Fridrich <zfridric@redhat.com>

--- p11-kit/delete-object.c
+++ p11-kit/delete-object.c
@@ -98,9 +98,9 @@ delete_object (const char *token_str)
 
 cleanup:
 	p11_kit_iter_free (iter);
-	p11_kit_modules_finalize (modules);
-	p11_kit_modules_release (modules);
 	p11_kit_uri_free (uri);
+	if (modules != NULL)
+		p11_kit_modules_finalize_and_release (modules);
 
 	return ret;
 }
