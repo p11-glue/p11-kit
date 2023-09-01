70b7ee8372fec89e1faea09fd673c3f4c6cb3a1d
Zoltan Fridrich <zfridric@redhat.com>
2023-09-01 13:35:48 +0200

    Add test for exporting public key
    Signed-off-by: Zoltan Fridrich <zfridric@redhat.com>

--- p11-kit/test-objects.sh
+++ p11-kit/test-objects.sh
@@ -45,34 +45,39 @@ Object: #6
     class: profile
     profile-id: public-certificates-token
 Object: #7
-    uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20LABEL;type=cert
+    uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20CERTIFICATE;type=cert
     class: certificate
     certificate-type: x-509
-    label: TEST LABEL
+    label: TEST CERTIFICATE
 Object: #8
+    uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20PUBLIC%20KEY;type=public
+    class: public-key
+    key-type: ec
+    label: TEST PUBLIC KEY
+Object: #9
     uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20LABEL;type=data
     class: data
     label: TEST LABEL
-Object: #9
+Object: #10
     uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=Public%20Capitalize%20Key;type=public
     class: public-key
     label: Public Capitalize Key
     private: false
-Object: #10
+Object: #11
     uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=Public%20prefix%20key;type=public
     class: public-key
     label: Public prefix key
     private: false
-Object: #11
+Object: #12
     uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20LABEL;type=data
     class: data
     label: TEST LABEL
-Object: #12
+Object: #13
     uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=Public%20Capitalize%20Key;type=public
     class: public-key
     label: Public Capitalize Key
     private: false
-Object: #13
+Object: #14
     uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=Public%20prefix%20key;type=public
     class: public-key
     label: Public prefix key
@@ -81,7 +86,7 @@ EOF
 
 "$abs_top_builddir"/p11-kit/p11-kit-testable list-objects -q "pkcs11:" > list.out
 
-echo 1..5
+echo 1..6
 
 : ${DIFF=diff}
 if ${DIFF} list.exp list.out > list.diff; then
@@ -113,7 +118,7 @@ EOF
 
 "$abs_top_builddir"/p11-kit/p11-kit-testable list-objects -q "pkcs11:type=data" > list.out
 
-echo 2..5
+echo 2..6
 
 : ${DIFF=diff}
 if ${DIFF} list.exp list.out > list.diff; then
@@ -126,15 +131,15 @@ fi
 
 cat > list.exp <<EOF
 Object: #0
-    uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20LABEL;type=cert
+    uri: pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20CERTIFICATE;type=cert
     class: certificate
     certificate-type: x-509
-    label: TEST LABEL
+    label: TEST CERTIFICATE
 EOF
 
-"$abs_top_builddir"/p11-kit/p11-kit-testable list-objects -q "pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20LABEL;type=cert" > list.out
+"$abs_top_builddir"/p11-kit/p11-kit-testable list-objects -q "pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20CERTIFICATE;type=cert" > list.out
 
-echo 3..5
+echo 3..6
 
 : ${DIFF=diff}
 if ${DIFF} list.exp list.out > list.diff; then
@@ -150,7 +155,7 @@ EOF
 
 "$abs_top_builddir"/p11-kit/p11-kit-testable list-objects -q "pkcs11:model=NONEXISTENT" > list.out
 
-echo 4..5
+echo 4..6
 
 : ${DIFF=diff}
 if ${DIFF} list.exp list.out > list.diff; then
@@ -174,15 +179,36 @@ X4wmG2aWDmRSHACW+4F3ojodSQwD1RnyagEpMfv1
 -----END CERTIFICATE-----
 EOF
 
-"$abs_top_builddir"/p11-kit/p11-kit-testable export-object -q "pkcs11:type=cert" > list.out
+"$abs_top_builddir"/p11-kit/p11-kit-testable export-object -q "pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20CERTIFICATE;type=cert" > list.out
 
-echo 5..5
+echo 5..6
 
 : ${DIFF=diff}
 if ${DIFF} list.exp list.out > list.diff; then
-	echo "ok 5 /objects/export-object"
+	echo "ok 5 /objects/export-object-cert"
 else
-	echo "not ok 5 /objects/export-object"
+	echo "not ok 5 /objects/export-object-cert"
+	sed 's/^/# /' list.diff
+	exit 1
+fi
+
+cat > list.exp <<EOF
+-----BEGIN PUBLIC KEY-----
+MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEn1LlwLN/KBYQRVH6HfIMTzfEqJOVztLe
+kLchp2hi78cCaMY81FBlYs8J9l7krc+M4aBeCGYFjba+hiXttJWPL7ydlE+5UG4U
+Nkn3Eos8EiZByi9DVsyfy9eejh+8AXgp
+-----END PUBLIC KEY-----
+EOF
+
+"$abs_top_builddir"/p11-kit/p11-kit-testable export-object -q "pkcs11:model=TEST%20MODEL;manufacturer=TEST%20MANUFACTURER;serial=TEST%20SERIAL;token=TEST%20LABEL;object=TEST%20PUBLIC%20KEY;type=public" > list.out
+
+echo 6..6
+
+: ${DIFF=diff}
+if ${DIFF} list.exp list.out > list.diff; then
+	echo "ok 6 /objects/export-object-pubkey"
+else
+	echo "not ok 6 /objects/export-object-pubkey"
 	sed 's/^/# /' list.diff
 	exit 1
 fi
