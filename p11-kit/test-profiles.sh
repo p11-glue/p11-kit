014325a9bd54acbc464a0628592ea773bbbe289f
Zoltan Fridrich <zfridric@redhat.com>
2023-09-26 13:12:44 +0200

    Test for nonexistent tokens in p11-kit commands
    Signed-off-by: Zoltan Fridrich <zfridric@redhat.com>

--- p11-kit/test-profiles.sh
+++ p11-kit/test-profiles.sh
@@ -33,4 +33,21 @@ EOF
 	fi
 }
 
-run test_list_profiles
+test_add_profile_nonexistent_token() {
+	cat > list.exp <<EOF
+EOF
+
+	if "$abs_top_builddir"/p11-kit/p11-kit-testable add-profile --profile=baseline-provider "pkcs11:token=NONEXISTENT" > list.out 2> err.out; then
+		assert_fail "expected to fail: p11-kit add-profile"
+	fi
+
+	: ${DIFF=diff}
+	if ! ${DIFF} list.exp list.out > list.diff; then
+		sed 's/^/# /' list.diff
+		assert_fail "output contains wrong results"
+	fi
+	assert_contains err.out "no matching token"
+}
+
+run test_list_profiles \
+    test_add_profile_nonexistent_token
