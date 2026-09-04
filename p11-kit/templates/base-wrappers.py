e27fa5e73e903e0851f579779f348747a479f8f5
Zoltan Fridrich <zfridric@redhat.com>
2026-09-04 14:04:09 +0200

    Improve template version checking
    Signed-off-by: Zoltan Fridrich <zfridric@redhat.com>

--- p11-kit/templates/base-wrappers.py
+++ p11-kit/templates/base-wrappers.py
@@ -3,6 +3,7 @@
     "function_argument": "{argument_type} {argument_name}",
     "call_lower": "return funcs->{function}",
     "call_lower_argument": "{argument_name}",
+    "function_version": "funcs->version",
     "function_body": """\
 static CK_RV
 {function_name} (CK_X_FUNCTION_LIST *self,
@@ -20,9 +21,7 @@ static CK_RV
 {{
 {indent}p11_virtual *virt = (p11_virtual *)self;
 {indent}CK_FUNCTION_LIST_3_2 *funcs = virt->lower_module;
-{indent}if (funcs->version.major < 3)
-{indent}{indent}return CKR_FUNCTION_NOT_SUPPORTED;
-{indent}{call_lower} ({call_lower_arglist});
+{version_check}{indent}{call_lower} ({call_lower_arglist});
 }}
     """,
     "entry_argument": "",       # unused
